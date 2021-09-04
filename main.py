2a5d36e444d695647a3f2c2578b3fa787dae2574
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-04 17:15:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Ball import Ball
 from score import Score
 import time
 
-# screen = Screen()
-# screen.setup(width=800, height=600)
-# screen.bgcolor("black")
-# screen.title("Pong Game")
-# screen.tracer(0)
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
 
 # r_paddle = Paddle((350,0))
 # l_paddle = Paddle((-350,0))
