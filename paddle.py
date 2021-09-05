38142a6692c751c10c5d06fe4aa23a8bcd152d45
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-05 21:28:56 +0530

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,14 +1,14 @@
 from turtle import Turtle
 
 
-# class Paddle(Turtle):
-#     def __init__(self,position):
-#         super().__init__()
-#         self.shape("square")
-#         self.shapesize(stretch_wid=5, stretch_len=1)
-#         self.color("white")
-#         self.penup()
-#         self.goto(position)
+class Paddle(Turtle):
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(position)
 
 
 #     def go_up(self):
